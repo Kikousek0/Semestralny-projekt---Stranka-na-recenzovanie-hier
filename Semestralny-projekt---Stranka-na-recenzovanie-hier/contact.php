e1aeab7c4c211a18c0c9c687f48ab4541994cbfa
Kristián Tóth <kikuskomikusko@gmail.com>
2026-04-06 18:45:56 +0200

    pridanie formulara

--- Semestralny-projekt---Stranka-na-recenzovanie-hier/contact.php
+++ Semestralny-projekt---Stranka-na-recenzovanie-hier/contact.php
@@ -1,6 +1,18 @@
         <?php require('parts/header.php'); ?>
 		<main class="contact-page">
             <h1>Kontakt</h1>
+            <form method="POST" action="contact.php" class="contact-form">
+                <label>Meno:</label>
+                <input type="text" name="name" required>
+
+                <label>Email:</label>
+                <input type="email" name="email" required>
+
+                <label>Správa:</label>
+                <textarea name="message" required></textarea>
+
+                <button type="submit">Odoslať</button>
+            </form>
 		</main>
 
         <?php require 'parts/footer.php' ?>
